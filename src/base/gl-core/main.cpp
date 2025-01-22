604111171c69cb83db7e8c2ff0746c47787b4769
set0xc3 <set0xc3@gmail.com>
2025-01-22 22:50:12 +0300

    backup

--- src/base/gl-core/main.cpp
+++ src/base/gl-core/main.cpp
@@ -1,10 +1,3 @@
-#include <boost/thread.hpp>
-#include <iostream>
-
-void hello() { std::cout << "Hello from thread!" << std::endl; }
-
 int main() {
-  boost::thread thrd(&hello);
-  thrd.join();
   return 0;
 }
